62bcb75e9016fe468dc191af3ffaa9ecc486fd86
Michael Weigle <michael.weigle@gmail.com>
2023-04-25 01:42:34 -0400

    perf: only call uname once per cmd

--- lib/utils.bash
+++ lib/utils.bash
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2034
 
 set -euo pipefail
 
@@ -6,3 +7,7 @@ fail() {
 	echo -e "asdf-awscli: $*"
 	exit 1
 }
+
+IFS=" " read -r -a OS_INFO <<<"$(uname --kernel-name --machine)"
+OS_NAME="${OS_INFO[0]}"
+OS_ARCH="${OS_INFO[0]}"
